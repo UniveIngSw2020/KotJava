a37821c3a01915a8ffa0a4e5bc3b905edf1488c5
danieledebattista <61229118+danieledebattista@users.noreply.github.com>
2020-11-17 11:57:34 +0100

    aggiunta prova request permission FINE LOCATION parti modificate :  manifest.xml  content_main.xml  MainActivity.java  Strings.xml

---
+++ Test1/app/src/main/java/com/example/test1/IntroductionActivity.java
@@ -0,0 +1,102 @@
+package com.example.test1;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class IntroductionActivity extends AppCompatActivity{
+    final int REQUEST_CODE = 123;
+    Button bt_permission = findViewById(R.id.button_accetta);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+
+        bt_permission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(IntroductionActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // when permission are not granted
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(IntroductionActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(IntroductionActivity.this);
+                        builder.setTitle("Grant those permission");
+                        builder.setMessage("read location");
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                ActivityCompat.requestPermissions(IntroductionActivity.this,
+                                        new String[]{
+                                                Manifest.permission.ACCESS_FINE_LOCATION
+                                        },
+                                        REQUEST_CODE);
+
+                            }
+                        });
+                        builder.setNegativeButton("cancel", null);
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    } else {
+                        ActivityCompat.requestPermissions(IntroductionActivity.this,
+                                new String[]{
+                                        Manifest.permission.ACCESS_FINE_LOCATION
+                                },
+                                REQUEST_CODE);
+
+                    }
+                } else {
+                    // when permission are already granted
+                    Toast.makeText(getApplicationContext(), "permission already granted", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == REQUEST_CODE){
+            if( grantResults.length > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+                //permission are granted
+                Toast.makeText(getApplicationContext(),"Permission Granted",Toast.LENGTH_SHORT).show();
+
+                //si passa alla prossima activity
+            }
+            else{
+                // permission are denied
+                Toast.makeText(getApplicationContext(),"Permission are denied",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
