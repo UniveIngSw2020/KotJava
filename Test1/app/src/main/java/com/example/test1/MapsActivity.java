62ed7059300649ec52e3e4a6864cc29e6d85e29a
DavideFavaro <874161@stud.unive.it>
2020-11-26 16:44:22 +0100

    Modifiche varie
    Modificato nav_graph.xml per centrare qualosa con l'applicazione.
    Aggiunto il codice per la creazione e gestione del menu sulla maps activity(ancora incompleto).

--- Test1/app/src/main/java/com/example/test1/MapsActivity.java
+++ Test1/app/src/main/java/com/example/test1/MapsActivity.java
@@ -2,7 +2,11 @@ package com.example.test1;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,6 +27,80 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+
+
+    //Creazione del menu della maps activity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_maps, menu);
+        return true;
+    }
+
+    //Gestione del click sulle varie voci del menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch(item.getItemId()) {
+            case R.id.help:
+                //Aprire sottomenu di help (Ho messo di seguito le voci, forse è uguale)
+                return true;
+            case R.id.guida:
+                /*Rimandare alla pagina con la guida/mostrare popup della guida
+                Intent intent = new Intent(this, "guida".class);
+                startActivity(intent);
+                */
+                return true;
+            case R.id.faq:
+                /*Rimandare alla pagina con le F.A.Q.
+                Intent intent = new Intent(this, "FAQ".class);
+                startActivity(intent);
+                */
+                return true;
+            case R.id.contatti:
+                /*Rimandare alla pagina dei contatti/mostrare popup dei contatti
+                Intent intent = new Intent(this, "contatti".class);
+                startActivity(intent);
+                */
+                return true;
+            case R.id.credits:
+                /*Rimandare alla pagina dei credits/mostrare popup dei credits
+                Intent intent = new Intent(this, "credits".class);
+                startActivity(intent);
+                */
+                return true;
+            case R.id.aggiornamento:
+                /*Rimandare alla pagina di aggiornamento
+                Intent intent = new Intent(this, "aggiornamento".class);
+                startActivity(intent);
+                */
+                return true;
+            case R.id.condividi:
+                //Copiare il link per la condivisione
+                return true;
+            case R.id.valuta:
+                //Something
+                return true;
+            case R.id.storico:
+                /*Rimandare alla pagina dello storico scansioni/mostrare popup dello storico
+                Intent intent = new Intent(this, "storico".class);
+                startActivity(intent);
+                */
+                return true;
+            case R.id.autoscan:
+                //Attivare/disattivare autoscan
+                return true;
+            case R.id.nascosti:
+                /*Rimandare alla pagina dei dispositivi nascosti/mostrare popup dei dispositivi
+                Intent intent = new Intent(this, "nascosti".class);
+                startActivity(intent);
+                */
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     /**
