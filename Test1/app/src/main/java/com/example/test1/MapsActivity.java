545a5c09de5dd5e90e76b11506a5877d39fd03a1
DavideFavaro <874161@stud.unive.it>
2020-12-22 18:08:44 +0100

    Compattato onCreateOptionsMenu

--- Test1/app/src/main/java/com/example/test1/MapsActivity.java
+++ Test1/app/src/main/java/com/example/test1/MapsActivity.java
@@ -247,8 +247,7 @@ private FusedLocationProviderClient fusedLocationClient;
     //Creazione del menu della maps activity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_maps, menu);
+        getMenuInflater().inflate(R.menu.menu_maps, menu);
         return true;
     }
 
